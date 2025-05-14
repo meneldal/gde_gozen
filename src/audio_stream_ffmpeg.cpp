48dbbe1e8760e90dd1b3e74b2d9efe951d11b7b6
Anzu Narita <anzu.narita@outlook.jp>
2025-05-14 18:08:36 +0900

    fix playback and add seeking

--- src/audio_stream_ffmpeg.cpp
+++ src/audio_stream_ffmpeg.cpp
@@ -1,65 +1,77 @@
 #include "audio_stream_ffmpeg.hpp"
 #include "ffmpeg.hpp"
 
-
 Ref<AudioStreamPlayback> AudioStreamFFmpeg::_instantiate_playback() const
 {
-    auto myplayback = memnew(AudioStreamFFmpegPlayback);
-    myplayback->m_stream=this;
-    myplayback->l_stereo=l_stereo;
-    myplayback->fill_buffer();
-    return myplayback;
+	auto myplayback = memnew(AudioStreamFFmpegPlayback);
+	myplayback->m_stream = this;
+	myplayback->l_stereo = l_stereo;
+	myplayback->fill_buffer();
+	return myplayback;
 }
 
 Ref<AudioStreamFFmpeg> AudioStreamFFmpeg::load_from_file(String a_path)
 {
-    UtilityFunctions::print("start reading from file\n");
-    auto mystream = memnew(AudioStreamFFmpeg);
-    mystream->m_format_ctx = avformat_alloc_context();
+	UtilityFunctions::print("start reading from file\n");
+	auto mystream = memnew(AudioStreamFFmpeg);
+	mystream->m_format_ctx = avformat_alloc_context();
 
-	if (!mystream->m_format_ctx) {
-        mystream->error = GoZenError::ERR_CREATING_AV_FORMAT_FAILED;
+	if (!mystream->m_format_ctx)
+	{
+		mystream->error = GoZenError::ERR_CREATING_AV_FORMAT_FAILED;
 		return mystream;
 	}
 
-	if (avformat_open_input(&mystream->m_format_ctx, a_path.utf8(), NULL, NULL)) {
-        mystream->error = GoZenError::ERR_OPENING_AUDIO;
+	if (avformat_open_input(&mystream->m_format_ctx, a_path.utf8(), NULL, NULL))
+	{
+		mystream->error = GoZenError::ERR_OPENING_AUDIO;
 		return mystream;
 	}
 
-	if (avformat_find_stream_info(mystream->m_format_ctx, NULL)) {
+	if (avformat_find_stream_info(mystream->m_format_ctx, NULL))
+	{
 		mystream->error = GoZenError::ERR_NO_STREAM_INFO_FOUND;
 		return nullptr;
 	}
 
-	for (int i = 0; i < mystream->m_format_ctx->nb_streams; i++) {
+	for (int i = 0; i < mystream->m_format_ctx->nb_streams; i++)
+	{
 		AVCodecParameters *av_codec_params = mystream->m_format_ctx->streams[i]->codecpar;
 
-		if (!avcodec_find_decoder(av_codec_params->codec_id)) {
+		if (!avcodec_find_decoder(av_codec_params->codec_id))
+		{
 			mystream->m_format_ctx->streams[i]->discard = AVDISCARD_ALL;
 			continue;
-		} else if (av_codec_params->codec_type == AVMEDIA_TYPE_AUDIO) {
-            //we got the right track with audio
+		}
+		else if (av_codec_params->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			// we got the right track with audio
 			mystream->m_stream = mystream->m_format_ctx->streams[i];
+			mystream->m_stream_idx = i;
 			break;
 		}
 	}
-    if (!mystream->m_format_ctx->streams){
-        //no audio stream found
-        mystream->error = GoZenError::ERR_OPENING_AUDIO;
-    }
+	if (!mystream->m_format_ctx->streams)
+	{
+		// no audio stream found
+		mystream->error = GoZenError::ERR_OPENING_AUDIO;
+	}
 
-    const AVCodec *l_codec_audio = avcodec_find_decoder(mystream->m_stream->codecpar->codec_id);
-	if (!l_codec_audio) {
+	const AVCodec *l_codec_audio = avcodec_find_decoder(mystream->m_stream->codecpar->codec_id);
+	if (!l_codec_audio)
+	{
 		UtilityFunctions::printerr("Couldn't find any codec decoder for audio!");
 		return mystream;
 	}
 
 	mystream->l_codec_ctx_audio = avcodec_alloc_context3(l_codec_audio);
-	if (mystream->l_codec_ctx_audio == NULL) {
+	if (mystream->l_codec_ctx_audio == NULL)
+	{
 		UtilityFunctions::printerr("Couldn't allocate codec context for audio!");
 		return mystream;
-	} else if (avcodec_parameters_to_context(mystream->l_codec_ctx_audio, mystream->m_stream->codecpar)) {
+	}
+	else if (avcodec_parameters_to_context(mystream->l_codec_ctx_audio, mystream->m_stream->codecpar))
+	{
 		UtilityFunctions::printerr("Couldn't initialize audio codec context!");
 		return mystream;
 	}
@@ -68,16 +80,16 @@ Ref<AudioStreamFFmpeg> AudioStreamFFmpeg::load_from_file(String a_path)
 	mystream->l_codec_ctx_audio->request_sample_fmt = AV_SAMPLE_FMT_S16;
 
 	// Open codec - Audio
-	if (avcodec_open2(mystream->l_codec_ctx_audio, l_codec_audio, NULL)) {
+	if (avcodec_open2(mystream->l_codec_ctx_audio, l_codec_audio, NULL))
+	{
 		UtilityFunctions::printerr("Couldn't open audio codec!");
 		return mystream;
 	}
 
-	
-	if (mystream->l_codec_ctx_audio->ch_layout.nb_channels <= 3) 
-    mystream->l_ch_layout = mystream->l_codec_ctx_audio->ch_layout;
+	if (mystream->l_codec_ctx_audio->ch_layout.nb_channels <= 3)
+		mystream->l_ch_layout = mystream->l_codec_ctx_audio->ch_layout;
 	else
-    mystream->l_ch_layout = AV_CHANNEL_LAYOUT_STEREO;
+		mystream->l_ch_layout = AV_CHANNEL_LAYOUT_STEREO;
 
 	auto response = swr_alloc_set_opts2(
 		&mystream->l_swr_ctx, &mystream->l_ch_layout, AV_SAMPLE_FMT_S16,
@@ -85,7 +97,8 @@ Ref<AudioStreamFFmpeg> AudioStreamFFmpeg::load_from_file(String a_path)
 		mystream->l_codec_ctx_audio->sample_fmt, mystream->l_codec_ctx_audio->sample_rate, 0,
 		nullptr);
 
-	if (response < 0) {
+	if (response < 0)
+	{
 		FFmpeg::print_av_error("Failed to obtain SWR context!", response);
 		avcodec_flush_buffers(mystream->l_codec_ctx_audio);
 		avcodec_free_context(&mystream->l_codec_ctx_audio);
@@ -93,7 +106,8 @@ Ref<AudioStreamFFmpeg> AudioStreamFFmpeg::load_from_file(String a_path)
 	}
 
 	response = swr_init(mystream->l_swr_ctx);
-	if (response < 0) {
+	if (response < 0)
+	{
 		FFmpeg::print_av_error("Couldn't initialize SWR!", response);
 		avcodec_flush_buffers(mystream->l_codec_ctx_audio);
 		avcodec_free_context(&mystream->l_codec_ctx_audio);
@@ -110,96 +124,173 @@ Ref<AudioStreamFFmpeg> AudioStreamFFmpeg::load_from_file(String a_path)
 
 	mystream->m_bytes_per_samples = av_get_bytes_per_sample(AV_SAMPLE_FMT_S16);
 	mystream->l_stereo = mystream->l_codec_ctx_audio->ch_layout.nb_channels >= 2;
-    return mystream;
+	return mystream;
 }
 
 void AudioStreamFFmpegPlayback::_start(double p_from_pos)
 {
-    is_playing=true;
+	is_playing = true;
 }
 
 void AudioStreamFFmpegPlayback::_stop()
 {
-    is_playing=false;
+	is_playing = false;
 }
 
 bool AudioStreamFFmpegPlayback::_is_playing() const
 {
-    return is_playing;
+	return is_playing;
 }
 
 double AudioStreamFFmpegPlayback::_get_playback_position() const
 {
-    return mixed / mix_rate;
+	return mixed / mix_rate;
 }
 
 void AudioStreamFFmpegPlayback::_seek(double p_position)
 {
+	buffer_fill = 0;
+	// convert seconds provided by the user to a timestamp in a correct base,
+	// then save it for later.
+	mixed = av_rescale_q(p_position * AV_TIME_BASE, AV_TIME_BASE_Q, m_stream->m_stream->time_base);
+
+	avcodec_flush_buffers(m_stream->l_codec_ctx_audio);
+
+	// Here we seek within given stream index and the correct timestamp
+	// for that stream. Using AVSEEK_FLAG_BACKWARD to make sure we're
+	// always *before* requested timestamp.
+	if (int err = av_seek_frame(m_stream->m_format_ctx, l_packet->stream_index, mixed, AVSEEK_FLAG_BACKWARD))
+	{
+		FFmpeg::print_av_error("audio_decoder: Error while seeking \n", err);
+	}
+	if (FFmpeg::get_frame(m_stream->m_format_ctx, m_stream->l_codec_ctx_audio, m_stream->m_stream->index, l_frame, l_packet))
+	{
+		// end of file
+	}
+	l_decoded_frame->format = AV_SAMPLE_FMT_S16;
+	l_decoded_frame->ch_layout = m_stream->l_ch_layout;
+	l_decoded_frame->sample_rate = l_frame->sample_rate;
+	l_decoded_frame->nb_samples = swr_get_out_samples(m_stream->l_swr_ctx, l_frame->nb_samples);
+
+	if (auto resp = (av_frame_get_buffer(l_decoded_frame, 0)) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't create new frame for swr!", resp);
+		av_frame_unref(l_frame);
+		av_frame_unref(l_decoded_frame);
+	}
+
+	if (auto resp = swr_config_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't config the audio frame!", resp);
+		av_frame_unref(l_frame);
+		av_frame_unref(l_decoded_frame);
+	}
+
+	if (auto resp = swr_convert_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't convert the audio frame!", resp);
+		av_frame_unref(l_frame);
+		av_frame_unref(l_decoded_frame);
+	}
+
+	size_t l_byte_size = l_decoded_frame->nb_samples * m_stream->m_bytes_per_samples;
+	if (m_stream->l_codec_ctx_audio->ch_layout.nb_channels >= 2)
+		l_byte_size *= 2;
+	
+
+	std::memcpy(buffer, l_decoded_frame->extended_data[0], l_byte_size);
+	buffer_fill += l_decoded_frame->nb_samples;
+	mix_rate = l_frame->sample_rate;
+	//check if we seek perfectly or not
+	if(l_packet->pts<mixed){
+		//we need to throw away data
+		auto skip_len = mixed-l_packet->pts;
+		buffer_fill-=skip_len;
+		std::memmove(buffer,buffer+skip_len,buffer_fill*4);
+	}	
+	av_frame_unref(l_frame);
+	av_frame_unref(l_decoded_frame);
+
 }
 
 int32_t AudioStreamFFmpegPlayback::_mix_resampled(AudioFrame *p_buffer, int32_t p_frames)
 {
-    // UtilityFunctions::print("start mix\n");
-    while(buffer_fill<p_frames)
-        fill_buffer();
-    //we have a buffer with buffer_fill values and need to provide p_frames
-    if(p_frames<=buffer_fill)
-    {
-        for(int i=0;i<p_frames;++i)
-            p_buffer[i] = AudioFrame{static_cast<float>(buffer[i].l)/32767.0f,static_cast<float>(buffer[i].r)/32767.0f};
-        buffer_fill-=p_frames;
-        std::memmove(buffer,buffer+p_frames*4,buffer_fill*4); //move memory back to beginning of buffer 4 bytes per sample
-        
-        mixed+=p_frames;
-        return p_frames;
-    }
-    return 0;
+	// UtilityFunctions::print("start mix\n");
+	while (buffer_fill < p_frames)
+		if (!fill_buffer())
+			break;
+	// we have a buffer with buffer_fill values and need to provide p_frames
+	if (p_frames <= buffer_fill)
+	{
+		for (int i = 0; i < p_frames; ++i)
+			p_buffer[i] = AudioFrame{static_cast<float>(buffer[i].l) / 32767.0f, static_cast<float>(buffer[i].r) / 32767.0f};
+		buffer_fill -= p_frames;
+		std::memmove(buffer, buffer + p_frames, buffer_fill * 4); // move memory back to beginning of buffer 4 bytes per sample
+
+		mixed += p_frames;
+		return p_frames;
+	}
+	// we still have some data to be sent over
+	else if (buffer_fill > 0)
+	{
+		for (int i = 0; i < buffer_fill; ++i)
+			p_buffer[i] = AudioFrame{static_cast<float>(buffer[i].l) / 32767.0f, static_cast<float>(buffer[i].r) / 32767.0f};
+		int32_t copied_frames = buffer_fill;
+		mixed += copied_frames;
+		buffer_fill = 0;
+		return copied_frames;
+	}
+	return 0;
 }
 
-void AudioStreamFFmpegPlayback::fill_buffer()
+bool AudioStreamFFmpegPlayback::fill_buffer()
 {
-    // UtilityFunctions::print("fill buffer\n");
-    if (FFmpeg::get_frame(m_stream->m_format_ctx, m_stream->l_codec_ctx_audio, m_stream->m_stream->index, l_frame, l_packet))
-			//error
-            return;
-        
-		// Copy decoded data to new frame
-		l_decoded_frame->format = AV_SAMPLE_FMT_S16;
-		l_decoded_frame->ch_layout = m_stream->l_ch_layout;
-		l_decoded_frame->sample_rate = l_frame->sample_rate;
-		l_decoded_frame->nb_samples = swr_get_out_samples(m_stream->l_swr_ctx, l_frame->nb_samples);
+	// UtilityFunctions::print("fill buffer\n");
+	if (FFmpeg::get_frame(m_stream->m_format_ctx, m_stream->l_codec_ctx_audio, m_stream->m_stream->index, l_frame, l_packet))
+	{
+		// end of file
+		return false;
+	}
 
-		if (auto resp =(av_frame_get_buffer(l_decoded_frame, 0)) < 0) {
-			FFmpeg::print_av_error("Couldn't create new frame for swr!", resp);
-			av_frame_unref(l_frame);
-			av_frame_unref(l_decoded_frame);
-			return;
-		}
+	// Copy decoded data to new frame
+	l_decoded_frame->format = AV_SAMPLE_FMT_S16;
+	l_decoded_frame->ch_layout = m_stream->l_ch_layout;
+	l_decoded_frame->sample_rate = l_frame->sample_rate;
+	l_decoded_frame->nb_samples = swr_get_out_samples(m_stream->l_swr_ctx, l_frame->nb_samples);
 
-		if (auto resp = swr_config_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0) {
-			FFmpeg::print_av_error("Couldn't config the audio frame!", resp);
-			av_frame_unref(l_frame);
-			av_frame_unref(l_decoded_frame);
-			return;
-		}
-
-		if (auto resp = swr_convert_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0) {
-			FFmpeg::print_av_error("Couldn't convert the audio frame!", resp);
-			av_frame_unref(l_frame);
-			av_frame_unref(l_decoded_frame);
-			return;
-		}
-
-		size_t l_byte_size = l_decoded_frame->nb_samples * m_stream->m_bytes_per_samples;
-		if (m_stream->l_codec_ctx_audio->ch_layout.nb_channels >= 2)
-			l_byte_size *= 2;
-
-        //UtilityFunctions::print("buffer size, ffmpeg buff",buffer_len," ",l_decoded_frame->nb_samples);
-        // std::memcpy(buffer+buffer_fill*4,l_decoded_frame->extended_data[0],l_byte_size);
-        std::memcpy(buffer+(buffer_fill)*4,l_decoded_frame->extended_data[0],l_byte_size);
-
-		buffer_fill+=l_decoded_frame->nb_samples;
-        mix_rate = l_frame->sample_rate;
+	if (auto resp = (av_frame_get_buffer(l_decoded_frame, 0)) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't create new frame for swr!", resp);
 		av_frame_unref(l_frame);
 		av_frame_unref(l_decoded_frame);
+		return false;
+	}
+
+	if (auto resp = swr_config_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't config the audio frame!", resp);
+		av_frame_unref(l_frame);
+		av_frame_unref(l_decoded_frame);
+		return false;
+	}
+
+	if (auto resp = swr_convert_frame(m_stream->l_swr_ctx, l_decoded_frame, l_frame) < 0)
+	{
+		FFmpeg::print_av_error("Couldn't convert the audio frame!", resp);
+		av_frame_unref(l_frame);
+		av_frame_unref(l_decoded_frame);
+		return false;
+	}
+
+	size_t l_byte_size = l_decoded_frame->nb_samples * m_stream->m_bytes_per_samples;
+	if (m_stream->l_codec_ctx_audio->ch_layout.nb_channels >= 2)
+		l_byte_size *= 2;
+
+	std::memcpy(buffer + buffer_fill, l_decoded_frame->extended_data[0], l_byte_size);
+
+	buffer_fill += l_decoded_frame->nb_samples;
+	mix_rate = l_frame->sample_rate;
+	av_frame_unref(l_frame);
+	av_frame_unref(l_decoded_frame);
+	return true;
 }
--- src/audio_stream_ffmpeg.hpp
+++ src/audio_stream_ffmpeg.hpp
@@ -4,101 +4,115 @@
 #include <godot_cpp/classes/audio_stream_playback_resampled.hpp>
 #include <godot_cpp/classes/audio_stream_playback.hpp>
 
-extern "C" {
-	#include <libavcodec/avcodec.h>
-	#include <libavcodec/codec.h>
-	#include <libavcodec/codec_id.h>
-	#include <libavcodec/packet.h>
-	
-	#include <libavdevice/avdevice.h>
-	
-	#include <libavformat/avformat.h>
-	
-	#include <libavutil/avassert.h>
-	#include <libavutil/avutil.h>
-	#include <libavutil/channel_layout.h>
-	#include <libavutil/dict.h>
-	#include <libavutil/display.h>
-	#include <libavutil/error.h>
-	#include <libavutil/frame.h>
-	#include <libavutil/hwcontext.h>
-	#include <libavutil/imgutils.h>
-	#include <libavutil/mathematics.h>
-	#include <libavutil/opt.h>
-	#include <libavutil/pixdesc.h>
-	#include <libavutil/rational.h>
-	#include <libavutil/timestamp.h>
-	
-	#include <libswresample/swresample.h>
-	#include <libswscale/swscale.h>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavcodec/codec.h>
+#include <libavcodec/codec_id.h>
+#include <libavcodec/packet.h>
+
+#include <libavdevice/avdevice.h>
+
+#include <libavformat/avformat.h>
+
+#include <libavutil/avassert.h>
+#include <libavutil/avutil.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/dict.h>
+#include <libavutil/display.h>
+#include <libavutil/error.h>
+#include <libavutil/frame.h>
+#include <libavutil/hwcontext.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/rational.h>
+#include <libavutil/timestamp.h>
+
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
 }
 
 #include "gozen_error.hpp"
 
 using namespace godot;
 
-class AudioStreamFFmpeg: public AudioStream{
+class AudioStreamFFmpeg : public AudioStream
+{
     GDCLASS(AudioStreamFFmpeg, AudioStream);
 
 public:
-    double _get_length() const override {return 0;}
-	bool _is_monophonic() const override {return true;}
-	Ref<AudioStreamPlayback> _instantiate_playback() const override;
+    double _get_length() const override { return 0; }
+    bool _is_monophonic() const override { return true; }
+    Ref<AudioStreamPlayback> _instantiate_playback() const override;
     static Ref<AudioStreamFFmpeg> load_from_file(String path);
-    virtual ~AudioStreamFFmpeg(){}//todo free ffmpeg stuff
+    virtual ~AudioStreamFFmpeg() {} // todo free ffmpeg stuff
     int error = 0;
     friend class AudioStreamFFmpegPlayback;
 
 protected:
-	static inline void _bind_methods() {
+    static inline void _bind_methods()
+    {
         ClassDB::bind_static_method("AudioStreamFFmpeg", D_METHOD("load_from_file", "a_file_path"), &AudioStreamFFmpeg::load_from_file);
-	}
+    }
+
 private:
-    AVFormatContext *m_format_ctx=nullptr;
-    AVStream *m_stream=nullptr;
-    AVCodecContext *l_codec_ctx_audio=nullptr;
-    int m_bytes_per_samples=0;
+    AVFormatContext *m_format_ctx = nullptr;
+    AVStream *m_stream = nullptr;
+    AVCodecContext *l_codec_ctx_audio = nullptr;
+    int m_bytes_per_samples = 0;
+    int m_stream_idx = 0;
     bool l_stereo = true;
     AVChannelLayout l_ch_layout;
     struct SwrContext *l_swr_ctx = nullptr;
 };
 
-class AudioStreamFFmpegPlayback: public AudioStreamPlaybackResampled{
+class AudioStreamFFmpegPlayback : public AudioStreamPlaybackResampled
+{
     GDCLASS(AudioStreamFFmpegPlayback, AudioStreamPlaybackResampled);
+
 public:
     void _start(double p_from_pos) override;
-	void _stop() override;
-	bool _is_playing() const override;
-	int32_t _get_loop_count() const override {return 0;}
-	double _get_playback_position() const override;
-	void _seek(double p_position) override;
-	int32_t _mix_resampled(AudioFrame *p_buffer, int32_t p_frames) override;
-    float _get_stream_sampling_rate() const override {return mix_rate;}
-    AudioStreamFFmpegPlayback(){
+    void _stop() override;
+    bool _is_playing() const override;
+    int32_t _get_loop_count() const override { return 0; }
+    double _get_playback_position() const override;
+    void _seek(double p_position) override;
+    int32_t _mix_resampled(AudioFrame *p_buffer, int32_t p_frames) override;
+    float _get_stream_sampling_rate() const override { return mix_rate; }
+    AudioStreamFFmpegPlayback()
+    {
         buffer = new sint16_stereo[buffer_len];
     }
-    virtual ~AudioStreamFFmpegPlayback(){
+    virtual ~AudioStreamFFmpegPlayback()
+    {
         delete[] buffer;
-    }//todo free ffmpeg stuff
+    } // todo free ffmpeg stuff
     friend class AudioStreamFFmpeg;
 
 protected:
-    static inline void _bind_methods() {
-	}
+    static inline void _bind_methods()
+    {
+    }
 
 private:
-    const AudioStreamFFmpeg* m_stream;
-    struct sint16_stereo{int16_t l; int16_t r;};
-    sint16_stereo* buffer;
-    size_t buffer_len = 4410000; //10s
-    size_t buffer_fill = 0; //number of samples current in buffer
-    bool is_playing=false;
+    const AudioStreamFFmpeg *m_stream;
+    struct sint16_stereo
+    {
+        int16_t l;
+        int16_t r;
+    };
+    sint16_stereo *buffer;
+    size_t buffer_len = 4410000; // 10s
+    size_t buffer_fill = 0;      // number of samples current in buffer
+    bool is_playing = false;
     AVFrame *l_frame = av_frame_alloc();
-	AVFrame *l_decoded_frame = av_frame_alloc();
-	AVPacket *l_packet = av_packet_alloc();
+    AVFrame *l_decoded_frame = av_frame_alloc();
+    AVPacket *l_packet = av_packet_alloc();
     bool l_stereo = true;
     uint32_t mixed = 0;
     uint32_t mix_rate = 44100;
 
-    void fill_buffer();
+    bool fill_buffer();
 };
